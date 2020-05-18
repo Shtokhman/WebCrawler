7e3714769d762f5deeeb5e4c790b2565daaf2763
Yuriy <36294649+Shtokhman@users.noreply.github.com>
2020-05-18 17:07:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/post.hpp>
 #include <boost/asio/strand.hpp>
+
 #include <atomic>
 #include <chrono>
-#include <cstdlib>
 #include <iomanip>
 #include <iostream>
 #include <memory>
@@ -16,10 +16,15 @@
 #include <thread>
 #include <vector>
 #include <map>
+#include <fstream>
+
+#include <gumbo.h>
+
+#include "gumbo_utils.hpp"
+
+#include "thread_safe_queue.hpp"
 
 
-
-namespace chrono = std::chrono;         // from <chrono>
 namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
 namespace net = boost::asio;            // from <boost/asio.hpp>
@@ -27,19 +32,19 @@ using tcp = net::ip::tcp;               // from <boost/asio/ip/tcp.hpp>
 
 //------------------------------------------------------------------------------
 
-std::vector<char const *> const &urls_large_data();
 
 // This structure aggregates statistics on all the sites
 class crawl_report {
+
     net::io_context &ioc_;
     net::strand<net::io_context::executor_type> strand_;
-    std::atomic<std::size_t> index_;
-    std::vector<char const *> const &hosts_;
+
     std::size_t count_ = 0;
+    std::size_t sz = 0;
 
 public:
-    explicit crawl_report(net::io_context &ioc)
-            : ioc_(ioc), strand_(ioc_.get_executor()), index_(0), hosts_(urls_large_data()) {
+    explicit crawl_report(net::io_context &ioc, size_t s)
+            : ioc_(ioc), strand_(ioc_.get_executor()), sz(s) {
     }
 
     // Run an aggregation function on the strand.
@@ -51,19 +56,10 @@ public:
                 [&, f] {
                     f(*this);
                     if (count_ % 10 == 0) {
-                        std::cerr << "Progress: " << count_ << " of " << hosts_.size() << "\n";
+                        std::cerr << "Progress: " << count_ << " of " << sz << "\n";
                     }
                     ++count_;
                 });
-    }
-
-    // Returns the next host to check
-    char const *
-    get_host() {
-        auto const n = index_++;
-        if (n >= hosts_.size())
-            return nullptr;
-        return hosts_[n];
     }
 
     // Counts the number of timer failures
@@ -118,12 +114,21 @@ class worker : public std::enable_shared_from_this<worker> {
     beast::flat_buffer buffer_; // (Must persist between reads)
     http::request<http::empty_body> req_;
     http::response<http::string_body> res_;
+    thread_safe_queue<std::string> &uqueue;
+    size_t max_pages;
+    size_t pages_indexed = 0;
 public:
     worker(worker &&) = default;
 
     // Resolver and socket require an io_context
-    worker(crawl_report &report, net::io_context &ioc) : report_(report), resolver_(net::make_strand(ioc)),
-                                                         stream_(net::make_strand(ioc)) {
+    worker(crawl_report &report, thread_safe_queue<std::string> &uq, size_t mp, net::io_context &ioc) : report_(report),
+                                                                                                        resolver_(
+                                                                                                                net::make_strand(
+                                                                                                                        ioc)),
+                                                                                                        uqueue(uq),
+                                                                                                        max_pages(mp),
+                                                                                                        stream_(net::make_strand(
+                                                                                                                ioc)) {
         // Set up the common fields of the request
         req_.version(11);
         req_.method(http::verb::get);
@@ -135,10 +140,12 @@ public:
     void run() { do_get_host(); }
 
     void do_get_host() {
+        if (pages_indexed >= max_pages) return;
+
         // Grab another host
-        auto const host = report_.get_host();
-        // nullptr means no more work
-        if (!host) return;
+        std::string host;
+        uqueue.pop(host);
+
         // The Host HTTP field is required
         req_.set(http::field::host, host);
         // Set up an HTTP GET request message
@@ -147,6 +154,7 @@ public:
     }
 
     void on_resolve(beast::error_code ec, tcp::resolver::results_type results) {
+        ++pages_indexed;
         if (ec) {
             report_.aggregate(
                     [](crawl_report &rep) {
@@ -194,6 +202,33 @@ public:
         http::async_read(stream_, buffer_, res_, beast::bind_front_handler(&worker::on_read, shared_from_this()));
     }
 
+    void extract_all_urls(std::string &code) {
+        EasyGumbo::Gumbo parser(code.c_str());
+        EasyGumbo::Gumbo::iterator iter = parser.begin();
+        while (iter != parser.end()) {
+            iter = std::find_if(iter, parser.end(), EasyGumbo::Tag(GUMBO_TAG_A));
+            if (iter == parser.end()) {
+                break;
+            }
+            EasyGumbo::Element titleA(*iter);
+            // std::cout << "***\n";
+            auto url = std::string(titleA.attribute("href")->value);
+            if (url.substr(0, 3) == "htt") {
+
+                auto index = url.find("/") + 2;
+                auto value_to_push = url.substr(index, url.size() - index);
+
+//                std::cout << std::setw(8) << "Url" << " : " << value_to_push << std::endl;
+
+                uqueue.push(value_to_push);
+            }
+
+            ++iter;
+        }
+
+    }
+
+
     void on_read(beast::error_code ec, std::size_t bytes_transferred) {
         boost::ignore_unused(bytes_transferred);
         if (ec) {
@@ -208,8 +243,17 @@ public:
                 [code](crawl_report &rep) {
                     ++rep.success;
                     ++rep.status_codes[code];
+
                 });
-        // std::cout << res_.body() << std::endl;
+
+        std::string response_body = res_.body();
+
+        GumboOutput *output = gumbo_parse(response_body.c_str());
+
+        extract_all_urls(response_body);
+
+        gumbo_destroy_output(&kGumboDefaultOptions, output);
+
         // Gracefully close the socket
         stream_.socket().shutdown(tcp::socket::shutdown_both, ec);
         stream_.close();
@@ -218,90 +262,44 @@ public:
     }
 };
 
-class timer {
-    using clock_type = chrono::system_clock;
+inline std::chrono::high_resolution_clock::time_point now() {
+    std::atomic_thread_fence(std::memory_order_seq_cst);
+    auto res_time = std::chrono::high_resolution_clock::now();
+    std::atomic_thread_fence(std::memory_order_seq_cst);
+    return res_time;
+}
 
-    clock_type::time_point when_;
-
-public:
-    using duration = clock_type::duration;
-
-    timer()
-            : when_(clock_type::now()) {
-    }
-
-    duration elapsed() const {
-        return clock_type::now() - when_;
-    }
-};
+template<class D>
+inline long long duration(const D &d) {
+    return std::chrono::duration_cast<std::chrono::seconds>(d).count();
+}
 
 
 std::vector<char const *> const &urls_large_data() {
-    static std::vector<char const *> const urls({
-                                                        "google.com",
-                                                        "facebook.com",
-                                                        "youtube.com",
-                                                        "yahoo.com",
-                                                        "baidu.com",
-                                                        "wikipedia.org",
-                                                        "qq.com",
-                                                        "linkedin.com",
-                                                        "taobao.com",
-                                                        "twitter.com",
-                                                        "live.com",
-                                                        "amazon.com",
-                                                        "sina.com.cn",
-                                                        "google.co.in",
-                                                        "hao123.com",
-                                                        "blogspot.com",
-                                                        "weibo.com",
-                                                        "wordpress.com",
-                                                        "yahoo.co.jp",
-                                                        "tmall.com",
-                                                        "bing.com",
-                                                        "ebay.com",
-                                                        "360.cn",
-                                                        "google.de",
-                                                        "sohu.com",
-                                                        "pinterest.com",
-                                                        "google.co.uk",
-                                                        "google.fr",
-                                                        "msn.com",
-                                                        "google.co.jp",
-                                                        "instagram.com",
-                                                        "google.com.br",
-                                                        "microsoft.com",
-                                                        "paypal.com",
-                                                        "google.ru",
-                                                        "google.es",
-                                                        "imdb.com",
-                                                        "apple.com",
-                                                        "google.it",
-                                                        "adcash.com",
-                                                        "stackoverflow.com",
-                                                        "amazon.co.jp",
-                                                        "neobux.com",
-                                                        "google.com.hk",
-                                                        "cnn.com",
-                                                        "google.com.mx",
-                                                        "gmw.cn",
-                                                        "reddit.com",
-                                                        "bbc.co.uk",
-                                                        "blogger.com",
-                                                        "google.ca",
-                                                        "go.com",
-                                                        "akamaihd.net",
-                                                        "alipay.com",
-                                                        "about.com",
-                                                        "people.com.cn",
-                                                        "googleusercontent.com",
-                                                        "wordpress.org",
-                                                        "alibaba.com",
-                                                        "google.com.tr",
-                                                        "aliexpress.com",
-                                                        "youku.com",
-                                                        "huffingtonpost.com"});
+    static std::vector<char const *> const urls({"go.com"});
     return urls;
+}
+
+
+class config {
+public:
+    std::string seed_webpage{};
+    size_t max_pages = 0;
+    size_t threads_count = 0;
+};
+
+void read_config(std::string &&filename, config &cfg) {
+    std::ifstream cf(filename);
+    std::string temp;
+    if (!cf.is_open()) {
+        std::cerr << "Failed to open configuration file " << filename << std::endl;
+        return;
+    }
+    cf >> cfg.seed_webpage;
+    getline(cf, temp);
+    cf >> cfg.threads_count;
+    getline(cf, temp);
+    cf >> cfg.max_pages;
 }
 
 
@@ -309,31 +307,40 @@ int main(int argc, char *argv[]) {
     // Check command line arguments.
     if (argc != 2) {
         std::cerr <<
-                  "Usage: http-crawl <threads>\n" <<
+                  "Usage: web-crawler <config-path>\n" <<
                   "Example:\n" <<
-                  "    http-crawl 100 1\n";
+                  "    web-crawler conf.txt\n";
         return -1;
     }
-    auto const threads = std::max<int>(1, std::atoi(argv[1]));
+
+
+    config cfg;
+
+    read_config(std::string(argv[1]), cfg);
+
+    thread_safe_queue<std::string> url_queue;
+    url_queue.push(cfg.seed_webpage);
+    auto const threads = cfg.threads_count;
     // The io_context is required for all I/O
     net::io_context ioc;
     // The work keeps io_context::run from returning
     auto work = net::make_work_guard(ioc);
     // The report holds the aggregated statistics
-    crawl_report report{ioc};
-    timer t;
+    crawl_report report{ioc, cfg.max_pages * cfg.threads_count};
+
     // Create and launch the worker threads.
     std::vector<std::thread> workers;
     workers.reserve(threads + 1);
+    auto s = now();
     for (int i = 0; i < threads; ++i)
         workers.emplace_back(
-                [&report] {
+                [&report, &url_queue, &cfg] {
                     // We use a separate io_context for each worker because
                     // the asio resolver simulates asynchronous operation using
                     // a dedicated worker thread per io_context, and we want to
                     // do a lot of name resolutions in parallel.
                     net::io_context ioc{1};
-                    std::make_shared<worker>(report, ioc)->run();
+                    std::make_shared<worker>(report, url_queue, cfg.max_pages, ioc)->run();
                     ioc.run();
                 });
     // Add another thread to run the main io_context which
@@ -352,7 +359,8 @@ int main(int argc, char *argv[]) {
         // Wait for the thread to exit
         thread.join();
     }
-    std::cout << "Elapsed time:    " << chrono::duration_cast<chrono::seconds>(t.elapsed()).count() << " seconds\n";
+    auto f = now();
+    std::cout << "Elapsed time:    " << duration(f - s) << " seconds\n";
     std::cout << report;
     return 0;
 }
